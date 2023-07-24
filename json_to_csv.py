133acf8c9804cc313e732bfb7194da84e01370c8
ubiratanfilho <ubfilho16@gmail.com>
2023-07-24 20:08:46 -0300

    Created .csv from json data

--- json_to_csv.py
+++ json_to_csv.py
@@ -5,22 +5,45 @@ import json
 import pandas as pd
 import os
 
-def json_to_csv(json_path: str, csv_path: str, _type: str):
+def json_images_to_csv(json_path: list, csv_path: str):
     """
     This function is used to convert JSON data to CSV format
     
     Arguments:
-        json_path: str
+        json_path: list
             Path to JSON file
         csv_path: str
             Path to CSV file
-        type: str
-            Type of JSON data, accepted values are 'annotations' and 'images'
     """
-    with open(json_path) as json_file:
-        json_data = json.load(json_file)
+    # elif _type == 'images':
+    #     data = json_data['images']
         
-    if _type == 'annotations':
+    #     df = pd.DataFrame(data)
+        
+    #     # now expand the column 'source'
+    #     df = pd.concat([df.drop(['source'], axis=1), df['source'].apply(pd.Series)], axis=1)
+    #     # now expand the column 'meta'
+    #     df = pd.concat([df.drop(['meta'], axis=1), df['meta'].apply(pd.Series)], axis=1)
+    # else:
+    #     raise ValueError('Invalid type')
+    # df.to_csv(csv_path, index=False)
+
+
+def json_annotations_to_csv(json_paths: list, csv_path: str):
+    """
+    This function is used to convert JSON data to CSV format
+    
+    Arguments:
+        json_path: list
+            Path to JSON file
+        csv_path: str
+            Path to CSV file
+    """
+    df_all = pd.DataFrame()
+    for json_path in json_paths:
+        with open(json_path) as json_file:
+            json_data = json.load(json_file)
+        
         data = json_data['annotations']
         
         df_people = pd.DataFrame(data)
@@ -37,47 +60,31 @@ def json_to_csv(json_path: str, csv_path: str, _type: str):
         df_people['file_name'] = df_people.index.astype(str) + '.png'
         # reset the index
         df_people = df_people.reset_index(drop=True)
+        # add to the df_all
+        df_all = pd.concat([df_all, df_people])
         
-        # get all the images
-        list_imgs = os.listdir('data/data')
-        
-        # create a dataframe
-        df_imgs = pd.DataFrame(list_imgs)
-        # rename the column
-        df_imgs = df_imgs.rename(columns={0: 'file_name'})
-        # create a new column 'label' and set it to 0
-        df_imgs['label'] = 0
-        
-        # merge the two dataframes and sum the column label
-        df = pd.concat([df_people, df_imgs]).groupby('file_name').sum()
-        df = df.reset_index() 
-        
-    elif _type == 'images':
-        data = json_data['images']
-        
-        df = pd.DataFrame(data)
-        
-        # now expand the column 'source'
-        df = pd.concat([df.drop(['source'], axis=1), df['source'].apply(pd.Series)], axis=1)
-        # now expand the column 'meta'
-        df = pd.concat([df.drop(['meta'], axis=1), df['meta'].apply(pd.Series)], axis=1)
-    else:
-        raise ValueError('Invalid type')
-    # df.to_csv(csv_path, index=False)
-        
+    # get all the images
+    list_imgs = os.listdir('data/train')
     
+    # create a dataframe
+    df_imgs = pd.DataFrame(list_imgs)
+    # rename the column
+    df_imgs = df_imgs.rename(columns={0: 'file_name'})
+    # create a new column 'label' and set it to 0
+    df_imgs['label'] = 0
+    
+    # merge the two dataframes and sum the column label
+    df = pd.concat([df_all, df_imgs]).groupby('file_name').sum()
+    df = df.reset_index() 
+    
+    # save the dataframe to csv
+    df.to_csv(csv_path, index=False)
+        
     return df
 
 json_paths = [
     'data/instances_train.json',
-    'data/instances_val.json',
-    'data/instances_test_nogt.json'
+    'data/instances_val.json'
 ]
 
-train_data = json_to_csv(json_paths[0], 'data/annotations_train.csv', 'annotations')
-val_data = json_to_csv(json_paths[1], 'data/annotations_val.csv', 'annotations')
-test_data = json_to_csv(json_paths[2], 'data/annotations_test.csv', 'annotations')
-
-# join all the dataframes
-df = pd.concat([train_data, val_data, test_data])
-print(df.shape)
+json_annotations_to_csv(json_paths, 'data/train.csv')
